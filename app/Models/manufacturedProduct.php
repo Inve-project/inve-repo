97a3ce55b0551feb028774737f2c7cb39122fd28
Ringo <benardringo18@gmail.com>
2023-05-16 07:47:28 -0700

    commited

---
+++ app/Models/manufacturedProduct.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class manufacturedProduct extends Model
+{
+    use HasFactory;
+}
