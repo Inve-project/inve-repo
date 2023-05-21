53c33b436f72c6b6bb2c4cdf854efa35fa9879ef
Ringo <benardringo18@gmail.com>
2023-05-21 03:49:32 -0700

    commited

--- app/Http/Controllers/Product/UserProductController.php
+++ app/Http/Controllers/Product/UserProductController.php
@@ -8,8 +8,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\UserProduct;
 use App\Models\Product;
-use App\Models\ProductUnits;
-use App\Models\ProductCategory;
 
 
 
@@ -33,6 +31,34 @@ class UserProductController extends Controller
             return view('Product.ListUserProduct',compact('data') );
 
             }
+
+            public function AddUserProduct(Request $request){
+                $id = $request->id;
+                $product = Product::find($id);
+                $quantity = 0;
+                $existingProduct = UserProduct::where('product_id', $id)->first();
+            
+                if ($existingProduct){
+                    return redirect()->back()->with('message', 'Product Exist');
+                } else {
+                    $data = new UserProduct;
+                    $data->User_id = Auth::id();
+                    $data->product_id = $product->id;
+                    $data->quantity = $quantity;
+                    $data->category = $product->category;
+                    $data->units = $product->units;
+                    $data->save();
+                    return redirect()->back()->with('message', 'Product added successfully');
+                }
+            }
+            
+
+            public function UserProductform(){
+
+                    $data = Product::all();
+                    return view('Product.UserProductform',compact('data') );
+        
+                    }
 
       
 }
